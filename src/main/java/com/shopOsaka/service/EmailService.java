2b4edf2743e18f3394617eda15f99d8e69bf9bcb
Rajendra4142 <C4142@oic.jp>
2026-05-29 12:47:39 +0900

    Initial Dockerized ShopOsaka Project

--- src/main/java/com/shopOsaka/service/EmailService.java
+++ src/main/java/com/shopOsaka/service/EmailService.java
@@ -7,12 +7,11 @@ import lombok.RequiredArgsConstructor;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import jakarta.mail.internet.MimeMessage;
-@Profile("!test")
+
 @Service
 @RequiredArgsConstructor
 public class EmailService {
--- src/test/java/com/shopOsaka/ShopOsakaApplicationTests.java
+++ src/test/java/com/shopOsaka/ShopOsakaApplicationTests.java
@@ -3,25 +3,10 @@ package com.shopOsaka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import com.shopOsaka.service.EmailService;
-import com.shopOsaka.service.OtpService;
-
-
-@SpringBootTest(properties = {
-	    "spring.datasource.url=jdbc:h2:mem:testdb",
-	    "spring.datasource.driver-class-name=org.h2.Driver",
-	    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-	    "spring.jpa.hibernate.ddl-auto=create-drop"
-	})
+@SpringBootTest
 class ShopOsakaApplicationTests {
-	
-	@MockitoBean
-	private EmailService emailService;
-	
-	@MockitoBean
-    private OtpService otpService;
+
 
 	@Test
 	void contextLoads() {
