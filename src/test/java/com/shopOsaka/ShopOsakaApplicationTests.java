5cf5e1d53ff9908378c19f4e899e8e02b430118a
Rajendra4142 <C4142@oic.jp>
2026-05-29 13:09:21 +0900

    add h2 test db for ci/cd

--- src/test/java/com/shopOsaka/ShopOsakaApplicationTests.java
+++ src/test/java/com/shopOsaka/ShopOsakaApplicationTests.java
@@ -2,10 +2,27 @@ package com.shopOsaka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.shopOsaka.service.EmailService;
+import com.shopOsaka.service.OtpService;
 
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	    "spring.datasource.url=jdbc:h2:mem:testdb",
+	    "spring.datasource.driver-class-name=org.h2.Driver",
+	    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+	    "spring.jpa.hibernate.ddl-auto=create-drop"
+	})
+
 class ShopOsakaApplicationTests {
+	
+	@MockitoBean
+	private EmailService emailService;
+	
+	@MockitoBean
+        private OtpService otpService;
+
 
 
 	@Test
